fe13c52e2c05faa1413ead1842dad26532782313
tepacki <tomtap1@wp.pl>
2024-01-10 16:38:42 +0100

    asdasd

--- Controllers/KsiazkiController.cs
+++ Controllers/KsiazkiController.cs
@@ -78,8 +78,28 @@ namespace Biblioteka.Controllers
             using(SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                using (SqlCommand command = new SqlCommand(query,))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@searchQuery", "%" + searchQuery + "%");
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id_ksiazka = Convert.ToInt32(reader["id_ksiazka"]);
+                            int id_kategoria = Convert.ToInt32(reader["id_kategoria"]);
+                            string autor = reader["autor"].ToString();
+                            string tytul = reader["tytul"].ToString();
+                            string wydawnictwo = reader["wydawnictwo"].ToString();
+                            int rok_wydania = Convert.ToInt32(reader["rok_wydania"]);
+                            int ilosc = Convert.ToInt32(reader["ilosc"]);
+
+                            Ksiazka ksiazka = new Ksiazka(id_ksiazka, id_kategoria, autor, tytul, wydawnictwo, rok_wydania, ilosc );
+
+                        }
+                    }
+                }
             }
+            return View("ListaKsiazek", listaKsiazek);
         }
     }
    
