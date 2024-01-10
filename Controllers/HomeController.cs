2cd338fbe2639bb8f2bcd228d909aad9cb70bf05
xWalian <lukasek008@gmail.com>
2024-01-10 13:44:11 +0100

    xd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Biblioteka.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
