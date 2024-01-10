760a8ec52619665f418895db2f07b661ed368d0d
tepacki <tomtap1@wp.pl>
2024-01-10 15:22:43 +0100

    asddasd

---
+++ Controllers/KsiazkiController.cs
@@ -0,0 +1,41 @@
+﻿using Biblioteka.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Biblioteka.Controllers
+{
+    public class KsiazkiController : Controller
+    {
+        public IActionResult ListaKsiazek()
+        {
+            string connectionString = "";
+            string query = "SELECT * FROM ksiazki";
+            List<Ksiazka> listaKsiazek = new List<Ksiazka>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id_ksiazka = Convert.ToInt32(reader["id_ksiazka"]);
+                            int id_kategoria = Convert.ToInt32(reader["id_kategoria"]);
+                            string autor = reader["autor"].ToString();
+                            string tytul = reader["tytul"].ToString();
+                            string wydawnictwo = reader["wydawnictwo"].ToString();
+                            int rok_wydania = Convert.ToInt32(reader["rok_wydania"]);
+                            int ilosc = Convert.ToInt32(reader["ilosc"]);
+
+                            Ksiazka ksiazka = new Ksiazka(id_ksiazka, id_kategoria, autor, tytul, wydawnictwo, rok_wydania, ilosc);
+                            listaKsiazek.Add(ksiazka);
+                        }
+                    }
+                }
+
+                return View(ListaKsiazek);
+            }
+        }
+    }
+}
---
+++ Models/Ksiazka.cs
@@ -0,0 +1,25 @@
+﻿namespace Biblioteka.Models
+{
+    public class Ksiazka
+    {
+        public int id_ksiazka { get; set; }
+        public int id_kategoria { get; set; }
+        public string autor { get; set; }
+        public string tytul {  get; set; } 
+        public string wydawnictwo {  get; set; }
+        public int rok_wydania {  get; set; }
+        public int ilosc {  get; set; }
+        
+        public Ksiazka(int id_ksiazka, int id_kategoria, string autor, string tytul, string wydawnictwo, int rok_wydania, int ilosc)
+        {
+            this.id_ksiazka = id_ksiazka;
+            this.id_kategoria = id_kategoria;
+            this.autor = autor;
+            this.tytul = tytul;
+            this.wydawnictwo = wydawnictwo;
+            this.rok_wydania = rok_wydania;
+            this.ilosc = ilosc;
+        }
+
+    }
+}
