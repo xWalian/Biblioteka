12c76b3902a15fae11898325a9fe9987233255e5
tepacki <tomtap1@wp.pl>
2024-01-11 20:10:21 +0100

    saddasd

---
+++ Models/Uzytkownik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteka.Models
+{
+	public class Uzytkownik
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int id_uzytkownik {  get; set; }
+		[Column]
+	}
+
+}
+
