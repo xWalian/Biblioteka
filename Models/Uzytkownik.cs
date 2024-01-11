c9656cccc2bfc47e8bf453f46a55b9d72f28612e
tepacki <tomtap1@wp.pl>
2024-01-11 20:13:13 +0100

    elo

--- Models/Uzytkownik.cs
+++ Models/Uzytkownik.cs
@@ -10,8 +10,22 @@ namespace Biblioteka.Models
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int id_uzytkownik {  get; set; }
-		[Column]
-	}
+		[Column(TypeName = "nvarchar(60")]
+		public string imie {  get; set; }
+        [Column(TypeName = "nvarchar(60")]
+        public string nazwisko { get; set; }
+        [Column(TypeName = "date")]
+        public string data_urodzenia { get; set; }
+        [Column(TypeName = "nvarchar(60")]
+        public string miasto { get; set; }
+        [Column(TypeName = "nvarchar(80")]
+        public string e-mail { get; set; }
+        [Column(TypeName = "nvarchar(80")]
+        public string haslo { get; set; }
+        [Column(TypeName = "nvarchar(15")]
+        public string rodzaj { get; set; }
+
+    }
 
 }
 
